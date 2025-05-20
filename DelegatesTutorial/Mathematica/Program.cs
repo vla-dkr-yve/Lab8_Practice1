5146a2b40c8cd948731a3f694f8a40c70eb708b8
Mateusz Jagieła <mateusz.jagiela@hotmail.com>
2025-05-20 10:26:07 +0200

    Initial code

---
+++ DelegatesTutorial/Mathematica/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, World");
+
+public class Integrator
+{
+    public double Integrate()
+    {
+        var start = 0.0;
+        var end = 1.0;
+        var step = 0.1;
+
+        var sum = 0.0;
+
+        for (var x = start; x < end; x += step)
+        {
+            var y = x * x;
+            sum += y * step;
+        }
+
+        return sum;
+    }
+}
