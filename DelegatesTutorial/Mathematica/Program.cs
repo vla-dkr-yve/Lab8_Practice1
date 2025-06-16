cb82daa64448ae1956545d6375b13a933fe022a9
vla-dkr-yve <vladkryve@gmail.com>
2025-06-16 20:42:18 +0200

    Finished tasks

--- DelegatesTutorial/Mathematica/Program.cs
+++ DelegatesTutorial/Mathematica/Program.cs
@@ -1,18 +1,18 @@
-﻿Console.WriteLine("Hello, World");
+﻿var intergator = new Integrator();
+
+
+Console.WriteLine(intergator.Integrate(0.0, 1, 0.01, x => x * x));
+Console.WriteLine(intergator.Integrate(0.0, 1, 0.01, x => Math.Sin(x)));
 
 public class Integrator
 {
-    public double Integrate()
+    public double Integrate(double start, double end, double step, Func<double, double> function)
     {
-        var start = 0.0;
-        var end = 1.0;
-        var step = 0.1;
-
         var sum = 0.0;
 
         for (var x = start; x < end; x += step)
         {
-            var y = x * x;
+            var y = function(x);
             sum += y * step;
         }
 
